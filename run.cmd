9f84ef2fc3b607cf28483ad56d224b05bc33b4f4
Matthias Pröll <proell.matthias@gmail.com>
2019-12-24 00:52:46 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,4 +1,16 @@
+@ECHO OFF
 cd /d %~dp0
+
+where python3
+if %errorlevel% == 1 goto nopythoninstalled
+if %errorlevel% == 0 goto run
+
+:nopythoninstalled
+echo "Es ist kein Python3 installiert!"
+pause
+exit 1
+
+:run
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './Get-OLCalendar.ps1'"
 python3 gcal.py
 exit 0
