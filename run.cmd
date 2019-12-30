eee1bdec76d16ba5b6a83724554c6abe538d1c18
Matthias Pröll <proell.matthias@gmail.com>
2019-12-30 23:53:44 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -2,11 +2,8 @@
 
 set mode=1
 
-
 cd /d %~dp0
-
 del events.csv events2.csv
-
 where python3
 if %errorlevel% == 1 goto nopythoninstalled
 if %errorlevel% == 0 goto run
@@ -29,7 +26,6 @@ exit 0
 :googletooutlook
 python3 "gcal.py" "2"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './Get-OLCalendar.ps1' -mode 2"
-
 exit 0
 
 :both
